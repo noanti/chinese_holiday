d09f525d3653f140a8a49604c5c90704081da0ae
李世琪 <lishiqi.v@bytedance.com>
2021-01-20 14:22:41 +0800

    增加2021年放假安排

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='chinese_holiday',
     author_email='noanti001@gmail.com',
-    version='0.0.1',
+    version='2021.1.1',
     packages=['chinese_holiday'],
     package_data={'chinese_holiday': ['festival_arrangements.yaml']},
     install_requires=[

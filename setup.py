1f3c374a150e8687d6bc1c52712f790b4e7f7580
李世琪 <lishiqi.v@bytedance.com>
2021-11-08 16:06:43 +0800

    update  to 2022

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='chinese_holiday',
     author_email='noanti001@gmail.com',
-    version='2021.1.1',
+    version='2022.0.0',
     packages=['chinese_holiday'],
     package_data={'chinese_holiday': ['festival_arrangements.yaml']},
     install_requires=[
